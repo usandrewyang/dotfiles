cd793f0ac2e79608a8471496c0ef6916278423ca
Andrew Y <usandrewyang@gmail.com>
2023-03-18 22:05:31 -0700

    [bugfix]Comment unused code in the config of treesitter

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,7 +1,7 @@
-local status_ok, treesitter = pcall(require, "nvim-treesitter")
+--[[ local status_ok, treesitter = pcall(require, "nvim-treesitter")
 if not status_ok then
     return
-end
+end ]]
 
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
@@ -9,7 +9,7 @@ if not status_ok then
 end
 
 configs.setup {
-    ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
+    ensure_installed = { "c", "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
     -- ensure_installed = "all", -- one of "all" or a list of languages
     ignore_install = { "" },                                                     -- List of parsers to ignore installing
     sync_install = false,                                                        -- install languages synchronously (only applied to `ensure_installed`)
