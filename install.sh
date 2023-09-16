bc7b313b8a0e2dda10127bdc5594d27044e0922d
andrew@oci <usandrewyang@gmail.com>
2023-09-16 20:09:34 +0000

    [feat] Add install script for all dotfiles

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
-#! /usr/bin/env bash
+#!/bin/bash
+# TODO A script to install all dotfiles. Not implemented yet
+
+echo "Installing .bashrc ..."
+
+echo "Installing .vimrc for vim ..."
+
+echo "Installing init.lua etc. for Neovim ..."
+
+echo "Installing .tmux.conf ..."
+
+echo "Installing global config for git ..."
 
